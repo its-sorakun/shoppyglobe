fee7153d3063f5f02c1c6097b6f680bde13d61d3
fetchthesauce <rukjaa908@gmail.com>
2026-09-02 12:54:36 +0530

    add descriptive file header to store configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,4 @@
+// store.js: Redux store configuration. Combines slices into the root reducer and sets up middleware automatically via Redux Toolkit.
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import searchReducer from './searchSlice';
