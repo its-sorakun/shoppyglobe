6d1b1bda2ccfeea95e8b3c1ed829bdaff77d305b
fetchthesauce <rukjaa908@gmail.com>
2026-09-03 18:40:12 +0530

    register auth reducer in global store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import searchReducer from './searchSlice';
+import authReducer from './authSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     search: searchReducer,
+    auth: authReducer,
   },
 });
